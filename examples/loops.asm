f42881891c52338bcc3792b53fcca9bdedf9a93b
marluxa95 <6677561-marlxx@users.noreply.gitlab.com>
2020-08-15 23:30:00 +0000

    Update loops.asm

--- examples/loops.asm
+++ examples/loops.asm
@@ -1,6 +1,6 @@
 .code   
     MOV 2 1     ; Moves value 2 to register 1
-    MOV 64 2     ; Moves value 6 to register 2
+    MOV 64 2     ; Moves value 64 to register 2
     ACC 2       ; Moves 2 to accumulator
     ADD 1       ; Adds ACC to register 2
     JNE 2 8     ; If ACC is not equal to R2 then goto 3
